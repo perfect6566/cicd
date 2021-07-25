ed9440d11b90c8233447ad507b3a86f519f76cbc
perfect6566 <perfect6566@126.com>
2021-07-25 18:54:59 +0800

    Update pipelineloadansible.groovy

--- vars/pipelineloadansible.groovy
+++ vars/pipelineloadansible.groovy
@@ -1,7 +1,30 @@
 #!groovy
-
-
-
+import org.cicd.ansible.ansible
+import org.cicd.tools.PrintTools
 def call(name){
  echo "hello ${name}"
+ 
+ pipeline {
+
+agent {
+label "awscloudagent"
+}
+stages{
+stage("build"){
+steps{
+script{
+def ansible = new ansible()
+ansible.AnsibleDeploy("127.0.0.1","-m ping")
+def tools = new PrintTools()
+PrintTools.PrintMes("我成功了")
+}
+
+}
+
+
+
+}
+}
+
+} 
 }
