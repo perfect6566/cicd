d072ade9e7cd3caf2e7ad63f4c1b16c85ba91257
perfect6566 <perfect6566@126.com>
2021-07-25 18:34:39 +0800

    Update pipelineloadansible.groovy

--- vars/pipelineloadansible.groovy
+++ vars/pipelineloadansible.groovy
@@ -2,34 +2,6 @@
 
 
 
-def call(hosts){
- 
-
-pipeline
-{
-     agent {  label "master"  }
-     
-     stages{
-         stage("build"){
-           steps {
-              script {
-             def ansible=new org.cicd.ansible()
-
-                   ansible.AnsibleDeploy("${hosts}","-m ping")      
-                 
-              }
-            }
-         }
-     }
-     
-     
-   post {
-        always {
-             script{
-                  def tools=new org.cicd.tools()
-                  tools.PrintMes("1.1.1.1")
-             }
-        }
-   }     
-}
+def call(name){
+ echo "hello ${name}"
 }
